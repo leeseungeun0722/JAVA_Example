2fd0955859d43bec722f9eaa0ab855ae3f258443
Seung eun <leeseungeun0722@gmail.com>
2022-03-14 18:56:25 +0900

    first commit

---
+++ Tutorial/src/Main1.java
@@ -0,0 +1,15 @@
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		
+		int intType = 100;
+		double doubleType = 150.5;
+		String stringType = "ÀÌ½ÂÀº";
+		
+		System.out.println(intType);
+		System.out.println(doubleType);
+		System.out.println(stringType);
+	}
+
+}
---
+++ Tutorial/src/Main2.java
@@ -0,0 +1,14 @@
+
+public class Main2 {
+	
+	final static double PI = 3.14232;
+	
+	public static void main(String[] args) {
+		
+		int r = 30;
+		
+		System.out.println(r * r * PI);
+	}
+	
+
+}
